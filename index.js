82154fcf521355dc4733d929a5a0ffbbd7311b16
Rasul Nekzad <rasulnekzad@Rasuls-MacBook-Air.local>
2023-06-14 18:18:29 -0400

    upload add row

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-
+function addNewRow() {
+    var rows = document.getElementById("grid-rows");
+    var cols = document.getElementById("grid-columns").children.length;
+    var newRow = document.createElement("tr");
+    for (let i = 0; i < cols; i++) {
+        var newCol = document.createElement("td");
+        newRow.appendChild(newCol);
+    }
+    rows.appendChild(newRow);
+}
