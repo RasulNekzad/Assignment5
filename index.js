17684e287eec37e74bdf74f7d7c0a75ebce6593f
Rasul Nekzad <rasulnekzad@Rasuls-MacBook-Air.local>
2023-06-14 19:06:31 -0400

    added addnewcol function

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 function addNewRow() {
     var rows = document.getElementById("grid-rows");
-    var cols = document.getElementById("grid-columns").children.length;
+    var numCols = document.getElementById("grid-columns").children.length;
     var newRow = document.createElement("tr");
-    for (let i = 0; i < cols; i++) {
+    for (let i = 0; i < numCols; i++) {
         var newCol = document.createElement("td");
         newRow.appendChild(newCol);
     }
     rows.appendChild(newRow);
+}
+
+function addNewColumn() {
+    var cols = document.getElementById("grid-columns");
+    var numRows = document.getElementById("grid-rows").children.length;
+    var rows = document.getElementById("grid-rows");
+    cols.appendChild(document.createElement("th"));
+    var allRows = rows.getElementsByTagName("tr");
+    for (let i = 0; i < allRows.length; i++) {
+        var newCol = document.createElement("td");
+        allRows[i].appendChild(newCol);
+    }
 }
